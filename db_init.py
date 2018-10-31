bce3aa7af54fb60fe3183f0bc17a248d68f3fa89
Prince Kelvin <kelvinwmblogs@gmail.com>
2018-10-31 10:53:06 +0300

    update travis.yml

--- db_init.py
+++ db_init.py
@@ -5,9 +5,9 @@ import os
 # db_url = os.getenv(url)
 
 def connection():
-    conn = psycopg2.connect(database="store", user="postgres", password="123", host="127.0.0.1", port="5432")
-    # DATABASE_URL = os.environ['DATABASE_URL']
-    # conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    # conn = psycopg2.connect(database="store", user="postgres", password="123", host="127.0.0.1", port="5432")
+    DATABASE_URL = os.environ['DATABASE_URL']
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
     return conn
 
