2a80ba06f96f88249e0a31f73cdcc589ea0a37cd
Prince Kelvin <kelvinwmblogs@gmail.com>
2018-11-01 10:32:04 +0300

    [#161642540] add DATABASE_URL to dbinit file

--- db_init.py
+++ db_init.py
@@ -2,9 +2,12 @@ import psycopg2
 import os
 
 
+# db_url = os.getenv(url)
+
 def connection():
-    DATABASE_URL = os.environ['DATABASE_URL']
-    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    conn = psycopg2.connect(database="store", user="postgres", password="123", host="127.0.0.1", port="5432")
+    # DATABASE_URL = os.environ['DATABASE_URL']
+    # conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
     return conn
 
