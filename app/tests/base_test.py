87b6af582b2c2b8c4d62f3a82c10a3938168a475
Prince Kelvin <kelvinwmblogs@gmail.com>
2018-11-01 10:59:58 +0300

    update base test

--- app/tests/base_test.py
+++ app/tests/base_test.py
@@ -18,9 +18,11 @@ class BaseTest(unittest.TestCase):
                                      "password": "25s#sssA4"}
         self.empty_login_data = {"email": "", "password": ""}
         self.invalid_login_data = {"username": "kevo", "email": "prince@gmail.com", "password": "12ds9vs33"}
-        self.result = self.app.post('/api/v2/auth/login', data=json.dumps(self.login_data),
+        self.result = self.app.post('/api/v2/auth/login', data=json.dumps({"email": "peterkelvin@storemanager.com",
+                                                                           "password": "25s#sssA4"}),
                                     content_type='application/json')
-        self.token = json.loads(self.result.data.decode('utf-8'))["Token"]
+        self.token_data = json.loads(self.result.data.decode('utf-8'))
+        self.token = self.token_data["Token"]
         self.headers = {'content-type': 'application/json', 'access-token': self.token}
         self.category = {"category": "klb"}
         self.category_update = {"category": "klb"}
