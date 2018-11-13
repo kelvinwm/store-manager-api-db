00f25d962ddd308a49449682ea23d0b856562b9c
Prince Kelvin <kelvinwmblogs@gmail.com>
2018-11-13 12:02:58 +0300

    update views file

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from flask_restful import Api
 from app.api.v2.views import (Products, Product, Home, UserSignup, UserLogin, UserLogout,
-                              AllUserInformation, SingleUserInformation, Categories,
+                              AllUserInformation, SingleUserInformation, CategoriesModel,
                               SingleCategory, Sales, Sale)
 
 version_2 = Blueprint("version_2", __name__)
@@ -14,7 +14,7 @@ api.add_resource(UserLogin, '/auth/login', endpoint="auth_login")
 api.add_resource(UserLogout, '/auth/logout', endpoint="auth_logout")
 api.add_resource(AllUserInformation, '/users', endpoint="users")
 api.add_resource(SingleUserInformation, '/users/<int:user_id>', endpoint="user")
-api.add_resource(Categories, '/category', endpoint="categories")
+api.add_resource(CategoriesModel, '/category', endpoint="categories")
 api.add_resource(SingleCategory, '/category/<int:category_id>', endpoint="category")
 api.add_resource(Sales, '/sales', endpoint="sales")
 api.add_resource(Sale, '/sales/<int:sale_id>', endpoint="sale")
